98353b8b7a59ae051ed0c139cda6c3cfc0ba9592
jessy413 <jessy413@ucla.edu>
2022-11-08 14:55:42 -0800

    diff locks

--- hash-table-v2.c
+++ hash-table-v2.c
@@ -78,45 +78,45 @@ void hash_table_v2_add_entry(struct hash_table_v2 *hash_table,
 							 const char *key,
 							 uint32_t value)
 {
-	static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+	static pthread_mutex_t mutex5 = PTHREAD_MUTEX_INITIALIZER;
 	static pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
 	static pthread_mutex_t mutex2 = PTHREAD_MUTEX_INITIALIZER;
 	static pthread_mutex_t mutex3 = PTHREAD_MUTEX_INITIALIZER;
 	static pthread_mutex_t mutex4 = PTHREAD_MUTEX_INITIALIZER;
 
-	pthread_mutex_lock(&mutex);
+	pthread_mutex_lock(&mutex1);
 	struct hash_table_entry *hash_table_entry = get_hash_table_entry(hash_table, key);
 	struct list_head *list_head = &hash_table_entry->list_head;
 	struct list_entry *list_entry = get_list_entry(hash_table, key, list_head);
-	pthread_mutex_unlock(&mutex);
+	pthread_mutex_unlock(&mutex1);
 
 	/* Update the value if it already exists */
 	if (list_entry != NULL)
 	{
-		int lock = pthread_mutex_lock(&mutex);
+		int lock = pthread_mutex_lock(&mutex2);
 		if (lock != 0)
 			exit(lock);
 		list_entry->value = value;
-		int unlock = pthread_mutex_unlock(&mutex);
+		int unlock = pthread_mutex_unlock(&mutex2);
 		if (unlock != 0)
 			exit(unlock);
 		return;
 	}
 
-	int lock = pthread_mutex_lock(&mutex);
+	int lock = pthread_mutex_lock(&mutex3);
 	if (lock != 0)
 		exit(lock);
 	list_entry = calloc(1, sizeof(struct list_entry));
-	pthread_mutex_unlock(&mutex);
+	pthread_mutex_unlock(&mutex3);
 
-	pthread_mutex_lock(&mutex);
+	pthread_mutex_lock(&mutex4);
 	list_entry->key = key;
 	list_entry->value = value;
-	pthread_mutex_unlock(&mutex);
+	pthread_mutex_unlock(&mutex4);
 
-	pthread_mutex_lock(&mutex);
+	pthread_mutex_lock(&mutex5);
 	SLIST_INSERT_HEAD(list_head, list_entry, pointers);
-	int unlock = pthread_mutex_unlock(&mutex);
+	int unlock = pthread_mutex_unlock(&mutex5);
 	if (unlock != 0)
 		exit(unlock);
 }
